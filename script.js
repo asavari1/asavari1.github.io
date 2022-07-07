f2533a90e33f03c6f87a5b6c525612abc45a7468
Omkar Kulkarni <kulkarni.ok.01@gmail.com>
2022-07-07 11:15:56 +0530

    k1n9

---
+++ script.js
@@ -0,0 +1,2 @@
+function pasuser(form) { if (form.id.value=="Asavari") { if (form.pass.value=="7776815255") {location=
+'clgflhaye8yq14700hxbkltssvkpiffgbjjccddhophfuditogyoyigfidyss5arw4wwr85@.html' } else { alert("Wrong Password") } } else { alert("Wrong Username") } }
